4f24595d26b13a5e90b49addb8ff6ca279255cbf
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2024-10-20 09:20:53 +0300

    Add gameBoard factory and intial board making fn/test

--- index.js
+++ index.js
@@ -11,4 +11,17 @@ const ship = (length = 0, numHits = 0) => {
   };
 };
 
-export { ship };
+const gameBoard = () => {
+  const horizontal = "abcdefghij";
+  const horizontalGrid = horizontal.split("");
+  const board = [];
+  // getting the board
+  horizontalGrid.forEach((item) => {
+    for (let i = 1; i < 11; i++) {
+      board.push({ cord: [item, i], occupied: false });
+    }
+  });
+
+  return { board };
+};
+export { ship, gameBoard };
--- index.test.js
+++ index.test.js
@@ -1,14 +1,19 @@
-import { ship } from "./index";
+import { ship, gameBoard } from "./index";
 test("test for hit fn", () => {
   const ship1 = ship(3);
   ship1.hit();
   expect(ship1.numHits).toBe(1);
 });
-test.only("test for isSunk fn", () => {
+test("test for isSunk fn", () => {
   const ship1 = ship(3);
   ship1.hit();
   ship1.hit();
   ship1.hit();
   ship1.hit();
   expect(ship1.isSunk()).toBe(true);
+});
+
+test.only("test for gameBoard", () => {
+  const gameBoard1 = gameBoard();
+  expect(gameBoard1.board.length).toBe(100);
 });

dbeb0faa68b11c1a181c768eaa169358df7f4554
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2024-10-20 08:01:47 +0300

    Add isSunk Fn/Test

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const ship = (length = 0, numHits = 0) => {
     hit: function () {
       this.numHits += 1;
     },
+    isSunk: function () {
+      return this.numHits >= this.length ? true : false;
+    },
   };
 };
 
--- index.test.js
+++ index.test.js
@@ -1,6 +1,14 @@
 import { ship } from "./index";
-test("test for hit fucntion", () => {
+test("test for hit fn", () => {
   const ship1 = ship(3);
   ship1.hit();
   expect(ship1.numHits).toBe(1);
+});
+test.only("test for isSunk fn", () => {
+  const ship1 = ship(3);
+  ship1.hit();
+  ship1.hit();
+  ship1.hit();
+  ship1.hit();
+  expect(ship1.isSunk()).toBe(true);
 });

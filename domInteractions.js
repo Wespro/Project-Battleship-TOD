a0e84546befb598be7d6bc140d6a7f6df9497960
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2024-10-21 11:16:24 +0300

    Add - - play and reset eventlistner - refactor Html boards to be implemnted by domInteraction - add animation to butons ,boards and links

--- domInteractions.js
+++ domInteractions.js
@@ -1,26 +1,97 @@
 import { Ship, Gameboard, Player } from "./index.js";
 
 export default (function () {
-  const computer = Player("Computer");
-  const player = Player("Player");
+  const play = document.querySelector(".play");
+  const shuffle = document.querySelector(".shuffle");
+  const reset = document.querySelector(".reset");
 
-  const youBoard = document.querySelector(".youBoard");
-  const computerBoard = document.querySelector(".computerBoard");
-  const fragment = document.createDocumentFragment();
-  const fragment2 = document.createDocumentFragment();
+  play.addEventListener("click", () => {
+    const computer = Player("Computer");
+    const player = Player("Player");
 
-  player.gameboard.board.forEach((cord) => {
-    const cell = document.createElement("div");
-    cell.classList.add(`${cord.cord[0]}${cord.cord[1]}`, "cell");
-    fragment.append(cell);
-    youBoard.append(fragment);
+    const yourBoardCont = document.querySelector(".yourBoardCont");
+    const youBoard = document.createElement("div");
+    const youBoardHeader = document.createElement("h2");
+    youBoard.classList.add("youBoard");
+    youBoardHeader.classList.add("youBoardHeader");
+    youBoardHeader.textContent = "Your Board";
+    const fragment = document.createDocumentFragment();
+
+    player.gameboard.board.forEach((cord) => {
+      const cell = document.createElement("div");
+      cell.classList.add(`${cord.cord[0]}${cord.cord[1]}`, "cell");
+
+      youBoard.append(cell);
+      fragment.append(youBoard);
+      yourBoardCont.append(youBoardHeader, fragment);
+    });
+
+    const computerBoardCont = document.querySelector(".computerBoardCont");
+
+    const computerBoard = document.createElement("div");
+    const computerBoardHeader = document.createElement("h2");
+    computerBoard.classList.add("computerBoard");
+    computerBoardHeader.classList.add("computerBoardHeader");
+    computerBoardHeader.textContent = "Computer Board";
+    const fragment2 = document.createDocumentFragment();
+
+    computer.gameboard.board.forEach((cord) => {
+      const cell = document.createElement("div");
+      cell.classList.add(`${cord.cord[0]}${cord.cord[1]}`, "cell");
+
+      computerBoard.append(cell);
+      fragment2.append(computerBoard);
+      computerBoardCont.append(computerBoardHeader, fragment2);
+    });
+    play.style.display = "none";
+    shuffle.style.display = "inline";
+    reset.style.display = "inline";
   });
 
-  computer.gameboard.board.forEach((cord) => {
-    const cell = document.createElement("div");
-    cell.classList.add(`${cord.cord[0]}${cord.cord[1]}`, "cell");
-    fragment2.append(cell);
-    computerBoard.append(fragment2);
+  reset.addEventListener("click", () => {
+    const computer = Player("Computer");
+    const player = Player("Player");
+
+    const yourBoardCont = document.querySelector(".yourBoardCont");
+
+    const youBoard = document.createElement("div");
+    const youBoardHeader = document.createElement("h2");
+    youBoard.classList.add("youBoard");
+    youBoardHeader.classList.add("youBoardHeader");
+    youBoardHeader.textContent = "Your Board";
+    const fragment = document.createDocumentFragment();
+
+    const computerBoardCont = document.querySelector(".computerBoardCont");
+
+    const computerBoard = document.createElement("div");
+    const computerBoardHeader = document.createElement("h2");
+    computerBoard.classList.add("computerBoard");
+    computerBoardHeader.classList.add("computerBoardHeader");
+    computerBoardHeader.textContent = "Computer Board";
+    const fragment2 = document.createDocumentFragment();
+
+    //reset dom elements
+    yourBoardCont.replaceChildren();
+    computerBoardCont.replaceChildren();
+    // append
+    player.gameboard.board.forEach((cord) => {
+      const cell = document.createElement("div");
+      cell.classList.add(`${cord.cord[0]}${cord.cord[1]}`, "cell");
+
+      youBoard.append(cell);
+      fragment.append(youBoard);
+      yourBoardCont.append(youBoardHeader, fragment);
+    });
+
+    computer.gameboard.board.forEach((cord) => {
+      const cell = document.createElement("div");
+      cell.classList.add(`${cord.cord[0]}${cord.cord[1]}`, "cell");
+
+      computerBoard.append(cell);
+      fragment2.append(computerBoard);
+      computerBoardCont.append(computerBoardHeader, fragment2);
+    });
+    play.style.display = "none";
   });
 
   return {};
